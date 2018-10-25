c230e18a2edde2fc42cd6a5f4081f74170e901e4
seachingbeyond <1355036599@qq.com>
2018-10-25 18:47:00 +0800

    增加处理生成了package目录的情况

--- compile_and_run.sh
+++ compile_and_run.sh
@@ -8,18 +8,27 @@ file=$1
 # 根据文件名来获取生成的class名称
 class=$(echo $file | awk -F '.' '{print $1}')
 
+# 生成的包
+package=$(head -1 Test.java | sed 's/package \(.*\);/\1/')
+
 echo "开始编译，请等待!!!"
 echo "------------------------------------"
 
-# 编译
-javac $file
+# 编译，并指定生成的class文件保存在当前目录
+javac -d . $file
 
 if [ $? -eq 0 ]; then
     echo "编译成功, 准备运行!!!"
     echo "------------------------------------"
 
+    # 判断是否生成了package目录，如果生成了package目录，就要修改class变量
+    if [ -e $package ]; then
+        class=$package.$class
+    fi
+
     # 运行
     java $class
+
     if [ $? -eq 0 ]; then
         echo "------------------------------------"
         echo "运行完毕!!!"
@@ -32,5 +41,11 @@ else
     echo "编译时出现错误!!!"
 fi
 
-# 删除当前目录下所有的*.class文件
-rm -f *.class
+# 如果生成了package目录，那么生成的class文件就在该目录中，删除所有class文件
+if [ -e $package ]; then
+    rm -rf $package 
+else
+    # 未生成package目录，删除当前目录下所有的*.class文件
+    rm -f *.class
+fi
+
